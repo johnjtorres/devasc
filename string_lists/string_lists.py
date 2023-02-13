25f75131ce0325a25a3af514d2b578d600da870f
John Torres <johnnyjamestorres@outlook.com>
2023-02-13 10:28:33 -0800

    Add link to exercise

--- string_lists/string_lists.py
+++ string_lists/string_lists.py
@@ -3,6 +3,8 @@
 """
 Ask the user for a string and print out whether this string is a palindrome or 
 not. (A palindrome is a string that reads the same forwards and backwards.)
+
+https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 """
 
 
