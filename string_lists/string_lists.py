014223161aa45c515e20d623d8a5acf21da3042b
John Torres <johnnyjamestorres@outlook.com>
2023-02-13 09:22:04 -0800

    Add string_lists solution

---
+++ string_lists/string_lists.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+"""
+Ask the user for a string and print out whether this string is a palindrome or 
+not. (A palindrome is a string that reads the same forwards and backwards.)
+"""
+
+
+def main():
+    phrase = input("Enter a word or phrase to check if it is a palindrome: ")
+    if is_palindrome(phrase):
+        print("That is a palindrome!")
+    else:
+        print("That is not a palindrome.")
+
+
+def is_palindrome(phrase):
+    only_alpha = [p.lower() for p in phrase if p.isalpha()]
+    return only_alpha == only_alpha[::-1]
+
+
+if __name__ == "__main__":
+    main()
