20a31701553cf26d8576b5629c55b98dc1bf03be
John Torres <johnnyjamestorres@outlook.com>
2023-02-16 14:51:48 -0800

    Add styling and additional route for name input

--- flask_framework/app.py
+++ flask_framework/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Hello, world!"
+@app.route("/<name>")
+def home(name="world"):
+    return render_template("hello.html", name=name)
